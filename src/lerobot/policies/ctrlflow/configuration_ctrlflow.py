969ea92d9ef7de55fd9ccf12bf9338f5aac7aa6a
liujg <jg548904337liu@gmail.com>
2026-04-04 22:36:33 +0800

    switch mix

--- src/lerobot/policies/ctrlflow/configuration_ctrlflow.py
+++ src/lerobot/policies/ctrlflow/configuration_ctrlflow.py
@@ -59,7 +59,7 @@ class CtrlFlowConfig(PreTrainedConfig):
     use_film_scale_modulation: bool = True
 
     # SDE/ODE 类型选择.
-    sde_type: str = "DS-SDE"
+    sde_type: str = "Mixed-W2KL"
     num_train_timesteps: int = 100
     beta_start: float = 0.0001
     beta_end: float = 0.02
